0e3f1e2926fe838ecaff6515c3508a6add9085fc
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-28 15:04:29 +0100

    header added to chart component

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, DatePicker } from 'antd';
 import { Line } from 'react-chartjs-2';
+
+const { RangePicker } = DatePicker;
 
 const data = {
   labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
@@ -19,8 +21,24 @@ function Chart() {
   return (
     <>
       <section className="chart mt-5">
-        <Row gutter={[16, 16]}>
-          <Col sm={24} md={24} lg={12} xl={16}>
+        <Row gutter={[18, 16]} align="stretch">
+          <Col sm={24} md={24} lg={12} xl={16} className="line-chart">
+            <div className="chart-header">
+              <div className="chart-header-date">
+                <h5>Today: {new Date().toDateString()}</h5>
+              </div>
+              <div className="chart-header-year">
+                <RangePicker picker="year" placeholder={['1 Jan', '1 Jun']} />
+              </div>
+              <div className="chart-header-buttons">
+                <button className="btn btn-md">
+                  <i className="fa fa-angle-left" aria-hidden="true"></i>
+                </button>
+                <button className="btn btn-md">
+                  <i className="fa fa-angle-right" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>
             <Line
               data={data}
               options={{
@@ -41,15 +59,15 @@ function Chart() {
                 <ul>
                   <li>
                     Pending Orders:
-                     <span className="yellow">10</span>
+                    <span className="yellow">10</span>
                   </li>
                   <li>
                     Reconcilled Orders:
-                     <span className="green">20</span>
+                    <span className="green">20</span>
                   </li>
                   <li>
                     Total Orders:
-                     <span className="blue">100</span>
+                    <span className="blue">100</span>
                   </li>
                 </ul>
               </Col>
@@ -58,15 +76,15 @@ function Chart() {
                 <ul>
                   <li>
                     Un-reconcilled Payments:
-                     <span className="yellow">10</span>
+                    <span className="yellow">10</span>
                   </li>
                   <li>
                     Reconcilled Payments:
-                     <span className="green">20</span>
+                    <span className="green">20</span>
                   </li>
                   <li>
                     Total Payments:
-                     <span className="blue">100</span>
+                    <span className="blue">100</span>
                   </li>
                 </ul>
               </Col>
