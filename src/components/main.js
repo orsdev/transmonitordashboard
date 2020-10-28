9a6c98b55fead84dee68b3da30f5fbdd9194b77f
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-28 10:56:25 +0100

    main-container className added to main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Layout } from 'antd';
 import Sidebar from './sidebar';
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from '@ant-design/icons';
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import Stats from './stats';
 import Payments from './payments';
 import Chart from './chart';
@@ -15,44 +12,47 @@ function Main() {
   const [collapsed, setCollapsed] = useState(false);
 
   useEffect(() => {
-    if (window.matchMedia("(max-width: 850px)").matches) {
+    if (window.matchMedia('(max-width: 850px)').matches) {
       setCollapsed(true);
     }
   }, []);
 
-
   function toggle() {
     setCollapsed(!collapsed);
-  };
+  }
 
   return (
     <>
       <main className="main">
-        <Layout>
-          <Sidebar collapsed={collapsed} />
-          <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }}>
-              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                className: 'trigger',
-                onClick: toggle,
-              })}
-            </Header>
-            <Content
-              className="site-layout-background"
-              style={{
-                margin: '24px 16px',
-                padding: 24
-              }}
-            >
-              <Stats />
-              <Chart />
-              <Payments />
-            </Content>
+        <div className="main-container">
+          <Layout>
+            <Sidebar collapsed={collapsed} />
+            <Layout className="site-layout">
+              <Header className="site-layout-background" style={{ padding: 0 }}>
+                {React.createElement(
+                  collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                  {
+                    className: 'trigger',
+                    onClick: toggle,
+                  }
+                )}
+              </Header>
+              <Content
+                className="site-layout-background"
+                style={{
+                  margin: '5px 16px',
+                  padding: 24,
+                }}>
+                <Stats />
+                <Chart />
+                <Payments />
+              </Content>
+            </Layout>
           </Layout>
-        </Layout>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
 export default Main;
